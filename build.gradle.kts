b1729df0d37ea6967161036cd895a0ddbc384cc1
acopier <acopierlive@gmail.com>
2025-11-21 17:30:32 +0100

    Added PaperMC Hangar autopublish

---
+++ build.gradle.kts
@@ -0,0 +1,98 @@
+import io.papermc.hangarpublishplugin.model.Platforms
+
+plugins {
+  id("java")
+  id("xyz.jpenilla.run-paper") version "3.0.2"
+  id("com.gradleup.shadow") version "9.2.2"
+  id("de.eldoria.plugin-yml.bukkit") version "0.8.0"
+  id("io.papermc.hangar-publish-plugin") version "0.1.3"
+}
+
+group = "io.github.acopier"
+version = "1.0-SNAPSHOT"
+val mcVersion = "1.21"
+
+repositories {
+  mavenCentral()
+  maven {
+    name = "papermc-repo"
+    url = uri("https://repo.papermc.io/repository/maven-public/")
+  }
+}
+
+dependencies {
+  compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+  implementation("com.cjcrafter:foliascheduler:0.7.2")
+}
+
+tasks {
+  runServer {
+    // Configure the Minecraft version for our task.
+    // This is the only required configuration besides applying the plugin.
+    // Your plugin"s jar (or shadowJar if present) will be used automatically.
+    minecraftVersion("1.21.8")
+  }
+}
+
+val targetJavaVersion = 21
+java {
+  val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+  sourceCompatibility = javaVersion
+  targetCompatibility = javaVersion
+  if (JavaVersion.current() < javaVersion) {
+    toolchain.languageVersion = JavaLanguageVersion.of(targetJavaVersion)
+  }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+  options.encoding = "UTF-8"
+
+  if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+    options.release.set(targetJavaVersion)
+  }
+}
+
+bukkit {
+  name = "RailDest"
+  description = "A plugin that allows fully automatic rail junctions."
+  website = "https://github.com/acopier/raildest"
+  author = "acopier"
+  main = "io.github.acopier.raildest.RailDestPlugin"
+  foliaSupported = true
+  apiVersion = mcVersion
+}
+
+tasks.shadowJar {
+  archiveFileName.set("RailDest-${project.version}.jar")
+  relocate(
+    "com.cjcrafter.foliascheduler",
+    "io.github.acopier.raildest.schedulers"
+  )
+}
+
+tasks.processResources {
+  val props = mapOf("version" to project.version.toString())
+  inputs.properties(props)
+  filteringCharset = "UTF-8"
+  filesMatching("plugin.yml") {
+    expand(props)
+  }
+}
+
+hangarPublish {
+  publications.register("plugin") {
+    version.set(project.version as String)
+    channel.set("Release")
+    id.set("RailDest")
+    apiKey.set(System.getenv("HANGAR_API_TOKEN"))
+    platforms {
+      register(Platforms.PAPER) {
+        jar.set(tasks.shadowJar.flatMap { it.archiveFile })
+        val versions: List<String> = "$mcVersion.x"
+          .split(",")
+          .map { it.trim() }
+        platformVersions.set(versions)
+      }
+    }
+  }
+}
